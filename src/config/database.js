4d0ddb5f174871e9ab00f3de7f0bac5a7b653f2f
Cody Chambers <sarg338@aim.com>
2025-06-27 17:52:51 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -7,11 +7,16 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env'
 
 // Create a new Pool instance with the connection details from our .env file
 const pool = new Pool({
+  /*
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT,*/
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false,
+  },
 });
 
 // Create a function to test the database connection
