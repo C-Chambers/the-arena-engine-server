37bbc5e9125fc4535a31c0c9844a5dfab4216bee
C-Chambers <sarg338@aim.com>
2025-06-29 00:42:45 -0500

    asdsadsad

--- src/game/engine.js
+++ src/game/engine.js
@@ -33,7 +33,8 @@ class Game {
 
   useSkill(skill, casterId, targetIds) {
     const casterPlayer = this.players[this.activePlayerId];
-    const opponentId = Object.keys(this.players).find(id => id !== this.activePlayerId);
+    // --- FIX: Ensure the opponent ID is found using a number-to-number comparison ---
+    const opponentId = Object.keys(this.players).find(id => parseInt(id, 10) !== this.activePlayerId);
     
     for (const type in skill.cost) {
       if (!casterPlayer.chakra[type] || casterPlayer.chakra[type] < skill.cost[type]) {
@@ -57,8 +58,12 @@ class Game {
         targets = this.players[opponentId].team.filter(c => c.isAlive);
       } else {
         targetIds.forEach(targetId => {
-            const target = this.players[targetPlayerId].team.find(c => c.instanceId === targetId);
-            if (target) targets.push(target);
+            // Find the correct player object to get the team from
+            const playerToTarget = Object.values(this.players).find(p => p.team.some(c => c.instanceId === targetId));
+            if(playerToTarget) {
+                const target = playerToTarget.team.find(c => c.instanceId === targetId);
+                if (target) targets.push(target);
+            }
         });
       }
 
@@ -88,18 +93,20 @@ class Game {
                 if(shield) {
                     if(shield.value >= damageToDeal) {
                         shield.value -= damageToDeal;
+                        this.log.push(`${target.name}'s shield absorbed ${damageToDeal} damage.`);
                         damageToDeal = 0;
                     } else {
                         damageToDeal -= shield.value;
+                        this.log.push(`${target.name}'s shield broke after absorbing ${shield.value} damage.`);
                         target.statuses = target.statuses.filter(s => s.type !== 'shield');
                     }
                 }
             }
             if (damageToDeal > 0) {
               target.currentHp -= damageToDeal;
+              this.log.push(`${target.name} took ${damageToDeal} damage.`);
             }
-            this.stats[this.activePlayerId].damageDealt += initialDamage;
-            this.log.push(`${target.name} took ${initialDamage} damage.`);
+            this.stats[this.activePlayerId].damageDealt += damageToDeal;
 
             if (target.currentHp <= 0) {
               target.isAlive = false;
@@ -178,7 +185,6 @@ class Game {
   nextTurn() {
     this.processTurnBasedEffects(this.players[this.activePlayerId]);
     this.turn++;
-    // --- FIX: Ensure player IDs are treated as numbers during comparison ---
     const playerIds = Object.keys(this.players).map(id => parseInt(id, 10));
     const currentIndex = playerIds.indexOf(this.activePlayerId);
     this.activePlayerId = playerIds[(currentIndex + 1) % playerIds.length];
